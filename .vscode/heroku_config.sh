cd94acb7dc5ec8ca5aebf3d9483866a63afa82e8
Luxolo Gqadu <Luwieluxolo@gmail.com>
2024-09-25 19:18:48 +0000

    heroku updated on heroku_config.sh

--- .vscode/heroku_config.sh
+++ .vscode/heroku_config.sh
@@ -9,7 +9,7 @@ echo Code Institute, 2021
 echo
 echo Get your Heroku API key by going to https://dashboard.heroku.com
 echo Go to Account Settings and click on Reveal to view your Heroku API key
-echo 
+echo
 
 if [[ -z "${HEROKU_API_KEY}" ]]; then
    echo Paste your Heroku API key here or press Enter to quit:
@@ -17,10 +17,11 @@ if [[ -z "${HEROKU_API_KEY}" ]]; then
    if [[ -z "${apikey}" ]]; then
       return 0
    fi
-   echo export HEROKU_API_KEY=${apikey} >> ~/.bashrc
-   echo Added the export. Refreshing the terminal.
-   . ~/.bashrc > /dev/null
-   echo Done!
+   echo "export HEROKU_API_KEY=${apikey}" >> ~/.zshrc
+   echo "Added the export. Refreshing the terminal."
+   . ~/.zshrc > /dev/null
+   echo "Done!"
+
 else
    echo API key is already set.
    echo
